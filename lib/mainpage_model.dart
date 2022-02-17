8955dd41abcc9b1d5f756f93fee2d4e4d61508d6
yuto90 <solidusgako4087@gmail.com>
2022-02-17 22:59:32 +0900

    admobの広告IDを設定

--- lib/mainpage_model.dart
+++ lib/mainpage_model.dart
@@ -60,11 +60,12 @@ class MainPageModel extends ChangeNotifier {
   }
 
   void initValue() {
-    print('init');
+    //print('init');
 
     // バナー広告をインスタンス化
     myBanner = BannerAd(
-      adUnitId: getTestAdBannerUnitId(),
+      //adUnitId: getTestAdBannerUnitId(),
+      adUnitId: 'ca-app-pub-8474156868822041/2299618878',
       size: AdSize.banner,
       request: const AdRequest(),
       listener: const BannerAdListener(),
@@ -80,10 +81,10 @@ class MainPageModel extends ChangeNotifier {
       // Android のとき
       testBannerUnitId =
           "ca-app-pub-3940256099942544/6300978111"; // Androidのデモ用バナー広告ID
-    } else if (Platform.isIOS) {
-      // iOSのとき
-      testBannerUnitId =
-          "ca-app-pub-3940256099942544/2934735716"; // iOSのデモ用バナー広告ID
+      //} else if (Platform.isIOS) {
+      //// iOSのとき
+      //testBannerUnitId =
+      //"ca-app-pub-3940256099942544/2934735716"; // iOSのデモ用バナー広告ID
     }
     return testBannerUnitId;
   }
