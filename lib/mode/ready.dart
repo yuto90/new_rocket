860eb28d59edf99f1eeee207319a14b72986bf9b
yuto90 <solidusgako4087@gmail.com>
2022-08-09 22:21:00 +0900

    ready画面に選択したレベルを表示

--- lib/mode/ready.dart
+++ lib/mode/ready.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:new_rocket/mainpage_model.dart';
 import '../size_config.dart';
 
 class Ready extends StatelessWidget {
-  final model;
-  Ready({this.model});
+  final MainPageModel model;
+  Ready({required this.model});
 
   @override
   Widget build(BuildContext context) {
@@ -12,42 +13,48 @@ class Ready extends StatelessWidget {
         ? Stack(
             children: [
               Align(
+                alignment: Alignment(0, -0.3),
+                child: Text(
+                  'L E V E L ' + model.selectedLevel.toString(),
+                  style: TextStyle(
+                    fontSize: SizeConfig.blockSizeVertical! * 2,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+              Align(
                 alignment: Alignment(0, -0.2),
-                child: model.gameHasStarted
-                    ? const SizedBox()
-                    : Text(
-                        'T A P  T O  P L A Y',
-                        style: TextStyle(
-                          fontSize: SizeConfig.blockSizeVertical! * 2,
-                          color: Colors.white,
-                        ),
-                      ),
+                child: Text(
+                  'T A P  T O  P L A Y',
+                  style: TextStyle(
+                    fontSize: SizeConfig.blockSizeVertical! * 2,
+                    color: Colors.white,
+                  ),
+                ),
               ),
               // 戻るボタン
               Align(
-                alignment: Alignment(0, 0.5),
-                child: model.gameHasStarted
-                    ? const SizedBox()
-                    : Container(
-                        height: SizeConfig.blockSizeVertical! * 5,
-                        width: SizeConfig.blockSizeHorizontal! * 40,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          color: Colors.black,
-                        ),
-                        child: OutlinedButton(
-                          onPressed: () {
-                            model.switchDisplay('top');
-                          },
-                          child: Text(
-                            'B A C K',
-                            style: TextStyle(
-                              fontSize: SizeConfig.blockSizeVertical! * 1.5,
-                              color: Colors.white,
-                            ),
-                          ),
-                        ),
+                alignment: Alignment(0, 0.3),
+                child: Container(
+                  height: SizeConfig.blockSizeVertical! * 5,
+                  width: SizeConfig.blockSizeHorizontal! * 40,
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    color: Colors.black,
+                  ),
+                  child: OutlinedButton(
+                    onPressed: () {
+                      model.switchDisplay('top');
+                    },
+                    child: Text(
+                      'B A C K',
+                      style: TextStyle(
+                        fontSize: SizeConfig.blockSizeVertical! * 1.5,
+                        color: Colors.white,
                       ),
+                    ),
+                  ),
+                ),
               ),
             ],
           )
