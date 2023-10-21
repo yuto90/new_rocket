a9eee0f0b17c464c8236ad926cc6dea537b50590
yuto90 <solidusgako4087@gmail.com>
2023-10-21 17:08:53 +0900

    雲を半透明にする

--- lib/objects/cloud.dart
+++ lib/objects/cloud.dart
@@ -10,11 +10,14 @@ class Cloud extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       //color: Colors.red,
-      child: Image(
-        image: AssetImage('lib/images/cloud.png'),
-        fit: BoxFit.cover,
-        height: vertical,
-        width: horizontal,
+      child: Opacity(
+        opacity: 0.8,
+        child: Image(
+          image: AssetImage('lib/images/cloud.png'),
+          fit: BoxFit.cover,
+          height: vertical,
+          width: horizontal,
+        ),
       ),
     );
   }
